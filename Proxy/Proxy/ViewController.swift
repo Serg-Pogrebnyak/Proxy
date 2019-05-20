caa42a811aa34f522287f56e3bb892dccc116ea1
Sergey Pogrebnyak <pogrebnyak.vetal1996@gmail.com>
2019-05-20 13:14:56 +0300

    empty project

---
+++ Proxy/Proxy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Proxy
+//
+//  Created by Sergey Pohrebnuak on 5/20/19.
+//  Copyright © 2019 Sergey Pohrebnuak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
